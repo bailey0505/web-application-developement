2485d443a93d6201b0356052259ff8e28c5fce60
bailey0505 <baileyrotellini1998@gmail.com>
2019-09-02 11:23:32 -0400

    Assignment 1 and Chapter 2 last example

--- assignment-1/index.js
+++ assignment-1/index.js
@@ -1,41 +1,59 @@
-var http = require('http');
-var fs = require('fs');
+//These are constants that never change
+const http = require('http');
+const fs = require('fs');
+const path = require("path");
 
-function serveStaticFile(res, path, contentType, responseCode) {
- if(!responseCode) responseCode = 200;
-       fs.readFile(__dirname + path, function(err,data) {
- if(err) {
-       res.writeHead(500, { 'Content-Type': 'text/plain' });
- res.end('500 - Internal Error');
- } else {
-         res.writeHead(responseCode,
- { 'Content-Type': contentType });
-      res.end(data);
-      }
-   });
-};
-
-http.createServer(function(req,res){
- // normalize url by removing querystring, optional
- // trailing slash, and making lowercase
- var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
-
-swith(path){ 
-
-	case '' :
-                        serveStaticFile(res, '/assignment-1/home.html', 'text/html');
-                         break;
-	case '/blue' :
-	                serveStaticFile(res, '/assignment-1/blue.html', 'text/html');
-	                break;
-	case '/green' :  
-	                serveStaticFile(res, '/assignment-1/green.html', 'text/html');
-			break; 
-	default '/home':
-			serveStaticFile(res, '/assignment-1/home.html', 'text/html');
-			break;
+//If url is not found, It ewill automatically spit out 404 error
+function send404(response){
+  response.writeHead(404, {'Content-Type': 'text/plain'});
+  response.write('Error 404: Resource not found.');
+  response.end();
 }
 
-}).listen(3001);
 
-console.log('Server started on localhost:3000; MoneyBag MoneyBag');
+//Configuring javascript and html files to be served
+const filelookup  = {
+	'.js': 'application/javascript',
+ 	 '.html': 'text/html'
+}
+
+//Main Function That will serve our Files and Create Server
+const server = http.createServer((req, res) => {
+  if(req.method == 'GET'){
+
+    let fileurl;
+    fileurl = req.url;
+//Using Math Random Javascript function to change homepage url to display green button link or blue button link 50/50	 
+    if(req.url == '/' && Math.random() > .5 ){
+      fileurl = 'homeb';
+  	
+   
+    }else if(req.url == '/'){
+	fileurl='homeg';
+
+    }
+
+    let filepath = path.resolve('./' + fileurl + '.html');
+
+    let fileExt = path.extname(filepath);
+    let filetype = filelookup[fileExt];
+
+    if(!filetype) {
+      send404(res);
+      return;
+    }
+
+    fs.exists(filepath, (exists) => {
+      if(!exists){
+        send404(res);
+        return;
+      }
+
+      res.writeHead(200, {'Content-Type': filetype});
+      fs.createReadStream(filepath).pipe(res);
+
+    });
+
+  }
+}).listen(3000);
+console.log("Server running at port 3000");
